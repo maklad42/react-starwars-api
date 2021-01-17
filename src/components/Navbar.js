d953aebad9aeef4d6e0aea98e7f25311781fb5ce
maklad42 <maklad42+git@gmail.com>
2021-01-17 17:15:38 +0900

    Completed the initial iterationn of the app. ToDo: Add sass and styled components. ToDo: Add front page text and bbackground image. ToDo: Add styling to the cards. 210117

---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Menu, Container } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+
+export default function Navbar() {
+  return (
+    <Menu inverted>
+      <Container>
+        <Link to="/">
+          <Menu.Item name="Star Wars API"></Menu.Item>
+        </Link>
+        <Link to="/people">
+          <Menu.Item name="people"></Menu.Item>
+        </Link>
+        <Link to="planets">
+          <Menu.Item name="planets"></Menu.Item>
+        </Link>
+      </Container>
+    </Menu>
+  );
+}
